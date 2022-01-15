89e092103fd2948d4b303d20189ff72930fd3379
assaf <assaf@post.bgu.ac.il>
2022-01-15 12:05:16 +0200

    publish

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'frameExtractor',
+    packages = ['frameExtractor'],
+    version = 'v1.0',  # Ideally should be same as your GitHub release tag varsion
+    description = 'extracting frames from mp4 using minimal space(great for remote files)',
+    author = 'Assaf Cohen',
+    author_email = 'assaf4356@gmail.com',
+    url = 'https://github.com/AssafCohen3/MP4EfficientFrameExtractor',
+    download_url = 'https://github.com/AssafCohen3/MP4EfficientFrameExtractor/archive/refs/tags/v1.0.tar.gz',
+    keywords = ['mp4', 'frame extractor'],
+    classifiers = [],
+)
